d6ea4ed7a8b2271e94a4ec2eb3dc5958953d4a34
Louise Lizé <louise.lize@icloud.com>
2025-11-05 15:33:09 +0100

    Implement location fetching

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,10 +1,41 @@
+import {
+  getCurrentPositionAsync,
+  requestForegroundPermissionsAsync,
+} from "expo-location";
+import { useEffect, useState } from "react";
 import { Text, View } from "react-native";
 import { styles } from "./Home.style";
+
 export function Home() {
+  const [location, setLocation] = useState(null);
+
+  useEffect(() => {
+    getUserLocation();
+  }, []);
+
+  async function getUserLocation() {
+    let { status } = await requestForegroundPermissionsAsync();
+    if (status !== "granted") {
+      setLocation({
+        latitude: 48.8566,
+        longitude: 2.3522,
+      });
+      return;
+    }
+
+    const location = await getCurrentPositionAsync();
+
+    setLocation({
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude,
+    });
+  }
+
   return (
     <>
       <View style={styles.meteo_basic}>
-        <Text style={styles.title}>Meteo Basic Component</Text>
+        <Text style={styles.title}> {location.latitude}</Text>
+        <Text style={styles.title}> {location.longitude}</Text>
       </View>
       <View style={styles.searchbar_container}></View>
       <View style={styles.meteo_advanced}></View>
