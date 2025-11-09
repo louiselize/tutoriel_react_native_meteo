16f7d54d2a4f7504df7f03a24542b3ff68b250d0
Louise Lizé <louise.lize@icloud.com>
2025-11-09 14:42:43 +0100

    Add back button to Forecast page with navigation functionality

--- pages/Forecast/Forecast.jsx
+++ pages/Forecast/Forecast.jsx
@@ -1,11 +1,23 @@
-import { useRoute } from "@react-navigation/native";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import { TouchableOpacity } from "react-native";
 import { AppText } from "../../components/AppText/AppText";
 import { Container } from "../../components/Container/Container";
+import { styles } from "./Forecast.style";
 
 export function Forecast({}) {
   const { params } = useRoute();
+  const navigation = useNavigation();
+
+  const backButton = (
+    <TouchableOpacity
+      onPress={() => navigation.goBack()}
+      style={styles.go_back_button}>
+      <AppText style={{ fontSize: 18 }}>←</AppText>
+    </TouchableOpacity>
+  );
   return (
     <Container>
+      {backButton}
       <AppText>Forecast at {params.city}</AppText>
     </Container>
   );
--- pages/Forecast/Forecast.style.js
+++ pages/Forecast/Forecast.style.js
@@ -1,3 +1,11 @@
 import { StyleSheet } from "react-native";
 
-export const styles = StyleSheet.create({});
+export const styles = StyleSheet.create({
+  go_back_button: {
+    padding: 10,
+    backgroundColor: "#4e8d96ff",
+    borderRadius: 8,
+    alignSelf: "flex-start",
+    margin: 10,
+  },
+});
